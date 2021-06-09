4aa7969c6b1403692a9d3e522c6298e6fbb1f48a
“Tony <adrprogramming@gmail.com>
2021-06-09 16:07:34 -0600

    commit tree 75be17d5c6a355ca762d3986ffbbab820e697f77

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,44 @@
+# require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner_array = data[season].select {|contestant|
+    contestant["status"] == "Winner"
+}
+
+winner = winner_array[0]
+winner_name = winner["name"].split(" ").first
+
+winner_name
+
 end
+
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.values.flatten.each {|contestant|
+    if contestant["occupation"] == occupation
+      return contestant["name"]
+    end
+}
 end
 
+# def get_contestant_name(data, occupation)
+#   target = data.values.flatten.find {|contestant|
+#     contestant["occupation"] == get_occupation
+#   }
+#   target["name"]
+# }
+# end ============== THIS IS THE SAME AS THE ABOVE
+
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each {|season, contestants|
+    contestants.each {|contestant|
+      if contestant["hometown"] == hometown
+        counter += 1
+      end
+  }
+}
+counter
 end
 
 def get_occupation(data, hometown)
@@ -15,5 +46,5 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  (data[season].map { |contestant| contestant["age"].to_f } / data[season].size).round
 end
